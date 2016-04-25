7a2b9d408a2863cf354ce364ceb385193daef086
ari <rizki.device@gmail.com>
2016-04-25 16:24:39 +0800

    update composer

---
+++ BigBlueButton.php
@@ -0,0 +1,24 @@
+<?php
+namespace arydeoblo\bigbluebutton;
+
+use Yii;
+use yii\base\Object;
+use yii\base\InvalidConfigException;
+
+Class BigBlueButton extends Object{
+
+	public $server_url;
+
+	public $server_secret;
+
+	public function init()
+	{
+		if($this->$server == null){
+			throw new InvalidConfigException('You mus set server url configuration');
+		}
+		if($this->$secret == null){
+			throw new InvalidConfigException('You mus set server secret configuration');
+		}
+	}
+
+}
