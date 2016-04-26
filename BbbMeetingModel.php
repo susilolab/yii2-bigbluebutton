db713666c40da459547d8050a2bca2a172de022f
ari <rizki.device@gmail.com>
2016-04-26 13:22:11 +0800

    add rules model

--- BbbMeetingModel.php
+++ BbbMeetingModel.php
@@ -8,4 +8,11 @@ class BbbMeetingModel extends ActiveRecord{
 	{
 		return 'bbb_meeting';
 	}
+
+	public function rules()
+	{
+		return [
+			[['name','meetingID','attendePW','moderatorPW','recordeMeeting'],'required']
+		];
+	}
 } 
