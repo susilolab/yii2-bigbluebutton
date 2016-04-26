27986d5fd9c075ca2746b1f553fb3e6aeb8163ef
ari <rizki.device@gmail.com>
2016-04-26 14:13:20 +0800

    fix model

--- BbbMeetingModel.php
+++ BbbMeetingModel.php
@@ -12,7 +12,18 @@ class BbbMeetingModel extends ActiveRecord{
 	public function rules()
 	{
 		return [
-			[['name','meetingID','attendePW','moderatorPW','recordeMeeting'],'required']
+			[['name','meetingID','attendePW','moderatorPW','recordedMeeting'],'required']
+		];
+	}
+
+	public function attributeLabels()
+	{
+		return [
+			'name' => 'Meeting Name',
+			'meetingID' => 'Meeting Token',
+			'attendePW' => 'Attendence Password',
+			'moderatorPW' => 'Moderator Password',
+			'recordedMeeting' => 'Recorded Meeting',
 		];
 	}
 } 
